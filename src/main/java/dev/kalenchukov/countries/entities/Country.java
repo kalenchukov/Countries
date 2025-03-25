959643a0cc775db8698b0645d2b446584318fd4b
Алексей Каленчуков <aleksey.kalenchukov@yandex.ru>
2025-03-25 17:06:52 +0300

    Добавлены комментарии классу Country

--- src/main/java/dev/kalenchukov/countries/entities/Country.java
+++ src/main/java/dev/kalenchukov/countries/entities/Country.java
@@ -28,31 +28,70 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
 
+/**
+ * Класс страны.
+ */
 public class Country
 {
+	/**
+	 * Буквенный код по стандарту ISO 3166 alpha 2.
+	 */
 	@NotNull
 	private final String iso3166Alpha2;
 
+	/**
+	 * Буквенный код по стандарту ISO 3166 alpha 3.
+	 */
 	@NotNull
 	private final String iso3166Alpha3;
 
+	/**
+	 * Цифровой код по стандарту ISO 3166 numeric.
+	 */
 	@NotNull
 	private final String iso3166Numeric;
 
+	/**
+	 * Буквенный код по стандарту FIPS.
+	 */
 	@NotNull
 	private final String fips;
 
+	/**
+	 * Название.
+	 */
 	@NotNull
 	private final String name;
 
+	/**
+	 * Столица.
+	 */
 	@NotNull
 	private final String capital;
 
+	/**
+	 * Континент расположения.
+	 */
 	@NotNull
 	private final String continent;
 
+	/**
+	 * Численность населения.
+	 */
 	private final int population;
 
+	/**
+	 * Конструктор для {@code Country}.
+	 *
+	 * @param iso3166Alpha2 буквенный код по стандарту ISO 3166 alpha 2.
+	 * @param iso3166Alpha3 буквенный код по стандарту ISO 3166 alpha 3.
+	 * @param iso3166Numeric цифровой код по стандарту ISO 3166 numeric.
+	 * @param fips буквенный код по стандарту FIPS.
+	 * @param name название.
+	 * @param capital столица.
+	 * @param continent континент расположения.
+	 * @param population численность населения.
+	 */
 	public Country(
 		@NotNull final String iso3166Alpha2,
 		@NotNull final String iso3166Alpha3,
@@ -74,53 +113,106 @@ public class Country
 		this.population = population;
 	}
 
+	/**
+	 * Возвращает буквенный код по стандарту ISO 3166 alpha 2.
+	 *
+	 * @return буквенный код по стандарту ISO 3166 alpha 2.
+	 */
 	@NotNull
 	public String getIso3166Alpha2()
 	{
 		return this.iso3166Alpha2;
 	}
 
+	/**
+	 * Возвращает буквенный код по стандарту ISO 3166 alpha 3.
+	 *
+	 * @return буквенный код по стандарту ISO 3166 alpha 3.
+	 */
 	@NotNull
 	public String getIso3166Alpha3()
 	{
 		return this.iso3166Alpha3;
 	}
 
+	/**
+	 * Возвращает цифровой код по стандарту ISO 3166 numeric.
+	 *
+	 * @return цифровой код по стандарту ISO 3166 numeric.
+	 */
 	@NotNull
 	public String getIso3166Numeric()
 	{
 		return this.iso3166Numeric;
 	}
 
+	/**
+	 * Возвращает буквенный код по стандарту FIPS.
+	 *
+	 * @return буквенный код по стандарту FIPS.
+	 */
 	@NotNull
 	public String getFips()
 	{
 		return this.fips;
 	}
 
+	/**
+	 * Возвращает название.
+	 *
+	 * @return название.
+	 */
 	@NotNull
 	public String getName()
 	{
 		return this.name;
 	}
 
+	/**
+	 * Возвращает столица.
+	 *
+	 * @return столица.
+	 */
 	@NotNull
 	public String getCapital()
 	{
 		return this.capital;
 	}
 
+	/**
+	 * Возвращает континент расположения.
+	 *
+	 * @return континент расположения.
+	 */
 	@NotNull
 	public String getContinent()
 	{
 		return this.continent;
 	}
 
+	/**
+	 * Возвращает численность населения.
+	 *
+	 * @return численность населения.
+	 */
 	public int getPopulation()
 	{
 		return this.population;
 	}
 
+	/**
+	 * Создаёт страну.
+	 *
+	 * @param iso3166Alpha2 буквенный код по стандарту ISO 3166 alpha 2.
+	 * @param iso3166Alpha3 буквенный код по стандарту ISO 3166 alpha 3.
+	 * @param iso3166Numeric цифровой код по стандарту ISO 3166 numeric.
+	 * @param fips буквенный код по стандарту FIPS.
+	 * @param name название.
+	 * @param capital столица.
+	 * @param continent континент расположения.
+	 * @param population численность населения.
+	 * @return страну.
+	 */
 	public static Country of(@NotNull final String iso3166Alpha2,
 							 @NotNull final String iso3166Alpha3,
 							 @NotNull final String iso3166Numeric,
@@ -133,6 +225,12 @@ public class Country
 		return new Country(iso3166Alpha2, iso3166Alpha3, iso3166Numeric, fips, name, capital, continent, population);
 	}
 
+	/**
+	 * Возвращает значение объекта в виде строки.
+	 *
+	 * @return строку.
+	 * @see Object#toString()
+	 */
 	@NotNull
 	@Override
 	public String toString()
@@ -149,6 +247,12 @@ public class Country
 			'}';
 	}
 
+	/**
+	 * Проверяет равенство объектов.
+	 *
+	 * @param object сравниваемый объект.
+	 * @return {@code true}, если объекты равны, иначе {@code false}.
+	 */
 	@Override
 	public boolean equals(final Object object)
 	{
@@ -165,6 +269,12 @@ public class Country
 		return Objects.equals(name, country.name);
 	}
 
+	/**
+	 * Возвращает значение объекта в виде числа.
+	 *
+	 * @return число.
+	 * @see Object#hashCode()
+	 */
 	@Override
 	public int hashCode()
 	{
